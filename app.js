36f7755c82fd0ffea6dfb9d309c791add1bf0216
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-07-03 21:47:08 +0900

    tried changing event coords in mobile

--- app.js
+++ app.js
@@ -86,9 +86,9 @@ function onMouseMove(event){
     let x = event.offsetX;
     let y = event.offsetY;
     if(isMobile){
-        const touches = event.changedTouches;
-        x = touches[0].screenX;
-        y = touches[0].screenY
+        const touch = event.originalEvent.touches[0];
+        x = touch.pageX;
+        y = touch.pageY;
     }
     if (canDraw){
         console.log(1);
@@ -110,8 +110,6 @@ function onMouseMove(event){
 }
 
 function changeColor(event){
-    event.preventDefault();
-    event.stopPropagation();
     let colorBtn = event.target;
     const color = colorBtn.style.backgroundColor;
     if(colorBtn.tagName == "IMG" || colorBtn.tagName == "SPAN"){
